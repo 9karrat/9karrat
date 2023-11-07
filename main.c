99f83bdbceb32618fee4eb4737847d921ccca94e
9karrat <150112249+9karrat@users.noreply.github.com>
2023-11-07 08:35:03 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,36 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+   int Balance = 10000;
+   int Withdrawal;
+   int Available_balance;
+
+   printf("Enter Amount:");
+   scanf("%d", &Withdrawal);
+   
+   
+}
+
+
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() 
+{
+   int Balance;
+   int Withdrawal;
+   int Available;
+   
+   Balance = 10000;
+
+   printf("Enter Amount:");
+   scanf("%d", &Withdrawal);
+   
+   Available = Balance - Withdrawal  ;
+   
+   printf("%d", & Available);
+   
+   
+}
